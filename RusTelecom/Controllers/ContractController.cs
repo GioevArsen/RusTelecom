0a0d7b90284fd64f80147ffeb6743d16a6132aed
GioevArsen <gioev.arsen15@list.ru>
2024-04-08 20:49:15 +0300

    Add project files.

---
+++ RusTelecom/Controllers/ContractController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RusTelecom.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RusTelecom.Controllers
+{
+    public class ContractController : Controller
+    {
+        private readonly ILogger<ContractController> _logger;
+
+        public ContractController(ILogger<ContractController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RusTelecom/Models/Contract.cs
@@ -0,0 +1,10 @@
+﻿namespace RusTelecom.Models
+{
+    public class Contract
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+
+    }
+}
